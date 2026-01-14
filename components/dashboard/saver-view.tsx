338ff657d590621f5ff945f6577c28e1b4330b4d
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-14 19:58:05 +0100

    refactor: Import VaultAnalyticsDashboard component in DashboardSaverView

--- components/dashboard/saver-view.tsx
+++ components/dashboard/saver-view.tsx
@@ -14,6 +14,7 @@ import { useState, useEffect } from "react";
 import { useVaultHealth } from "@/lib/hooks/useVaultHealth";
 import Link from "next/link";
 import PolicyConfig from '@/components/dashboard/policy-config';
+import { VaultAnalyticsDashboard } from './VaultAnalyticsDashboard';
 
 export function DashboardSaverView() {
         // Timer for stable current time in render
