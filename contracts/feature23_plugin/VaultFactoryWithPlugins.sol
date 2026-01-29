b9d977105becf6ddf42562ff56f7d2e2fe52132f
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:27:57 +0100

    chore: add VaultFactoryWithPlugins contract and initial test for Automated Yield Integration

---
+++ contracts/feature23_plugin/VaultFactoryWithPlugins.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title VaultFactoryWithPlugins
+/// @notice Factory for deploying plugin-enabled vaults
+contract VaultFactoryWithPlugins {
+    // ...core logic to be implemented...
+}
---
+++ test/feature21_yield.test.js
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Feature #21: Automated Yield Integration", function () {
+  // ...test setup and cases to be implemented...
+});
