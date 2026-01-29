a9144b6b64a9e51bd4c60ab8bf0d43377489df06
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:45:37 +0100

    feat: implement VaultFactoryWithYield contract for deploying yield-enabled vaults

--- contracts/feature21_yield/VaultFactoryWithYield.sol
+++ contracts/feature21_yield/VaultFactoryWithYield.sol
@@ -3,6 +3,28 @@ pragma solidity ^0.8.20;
 
 /// @title VaultFactoryWithYield
 /// @notice Factory for deploying yield-enabled vaults
+import "./SpendVaultWithYield.sol";
+
 contract VaultFactoryWithYield {
-    // ...core logic to be implemented...
+    address public yieldStrategyManager;
+    address[] public allVaults;
+
+    event VaultDeployed(address indexed owner, address vault, uint256 timestamp);
+
+    constructor(address _yieldStrategyManager) {
+        yieldStrategyManager = _yieldStrategyManager;
+    }
+
+    /// @notice Deploy a new SpendVaultWithYield for the caller
+    function createVault() external returns (address vault) {
+        vault = address(new SpendVaultWithYield(yieldStrategyManager));
+        allVaults.push(vault);
+        emit VaultDeployed(msg.sender, vault, block.timestamp);
+        // Optionally: register with YieldStrategyManager
+    }
+
+    /// @notice Get all deployed vaults
+    function getAllVaults() external view returns (address[] memory) {
+        return allVaults;
+    }
 }
