4b28d45a795310407e93da43e9cb313e3918f87a
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:56:34 +0100

    feat: add SpendVaultWithDID contract with basic structure for DID-based identity and permissions

---
+++ contracts/feature_did/SpendVaultWithDID.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title SpendVaultWithDID
+/// @notice Vault with DID-based identity and permissions
+contract SpendVaultWithDID {
+    // ...core logic to be implemented...
+}
--- contracts/feature_oracle_nft/SpendVaultWithNFTCollateral.sol
+++ contracts/feature_oracle_nft/SpendVaultWithNFTCollateral.sol
@@ -3,6 +3,32 @@ pragma solidity ^0.8.20;
 
 /// @title SpendVaultWithNFTCollateral
 /// @notice Vault with NFT collateralization support
+interface INFTCollateralManager {
+    function depositNFT(address nft, uint256 tokenId) external;
+    function withdrawNFT(address nft, uint256 tokenId) external;
+}
+
 contract SpendVaultWithNFTCollateral {
-    // ...core logic to be implemented...
+    address public owner;
+    address public collateralManager;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor(address _collateralManager) {
+        owner = msg.sender;
+        collateralManager = _collateralManager;
+    }
+
+    /// @notice Deposit an NFT as collateral
+    function depositNFT(address nft, uint256 tokenId) external onlyOwner {
+        INFTCollateralManager(collateralManager).depositNFT(nft, tokenId);
+    }
+
+    /// @notice Withdraw an NFT collateral
+    function withdrawNFT(address nft, uint256 tokenId) external onlyOwner {
+        INFTCollateralManager(collateralManager).withdrawNFT(nft, tokenId);
+    }
 }
