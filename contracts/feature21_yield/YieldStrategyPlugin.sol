408c59c4061ce3158235e07e1f8f7ff81b3b96b6
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:25:52 +0100

    chore: add YieldStrategyPlugin contract for pluggable strategy integration and create initial documentation for on-chain insurance coverage

---
+++ contracts/feature21_yield/YieldStrategyPlugin.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title YieldStrategyPlugin
+/// @notice Pluggable strategy contract for protocol integration
+contract YieldStrategyPlugin {
+    // ...core logic to be implemented...
+}
