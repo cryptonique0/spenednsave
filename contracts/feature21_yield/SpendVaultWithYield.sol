70cce84d453416fca5cf771aba81d700361cedf2
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:24:01 +0100

    chore: add initial implementation for automated yield integration feature

---
+++ contracts/feature21_yield/SpendVaultWithYield.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title SpendVaultWithYield
+/// @notice Vault with automated yield integration
+contract SpendVaultWithYield {
+    // ...core logic to be implemented...
+}
