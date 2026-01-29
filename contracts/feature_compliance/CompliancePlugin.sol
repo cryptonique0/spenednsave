3ec2f02635ce1de5ac8e5bd4d7a383f371416e6a
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:59:12 +0100

    feat: implement CompliancePlugin and SpendVaultWithCompliance contracts with KYC/AML functionality

--- contracts/feature_compliance/CompliancePlugin.sol
+++ contracts/feature_compliance/CompliancePlugin.sol
@@ -3,6 +3,22 @@ pragma solidity ^0.8.20;
 
 /// @title CompliancePlugin
 /// @notice KYC/AML plugin module for regulated DAOs/treasuries
+
 contract CompliancePlugin {
-    // ...core logic to be implemented...
+    // account => KYC/AML status
+    mapping(address => bool) public isCompliant;
+
+    event ComplianceStatusUpdated(address indexed account, bool status, uint256 timestamp);
+
+    /// @notice Set compliance status (admin only, placeholder)
+    function setComplianceStatus(address account, bool status) external {
+        // TODO: admin access control
+        isCompliant[account] = status;
+        emit ComplianceStatusUpdated(account, status, block.timestamp);
+    }
+
+    /// @notice Check compliance status
+    function checkCompliance(address account) external view returns (bool) {
+        return isCompliant[account];
+    }
 }
