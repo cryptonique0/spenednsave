1af595427bbcfe43e7559a381872345b8f80d494
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:32:58 +0100

    chore: add VaultFactoryWithDAOControl contract and tests for DAO governance integration

---
+++ test/feature25_dao.test.js
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Feature #25: DAO Governance Integration", function () {
+  // ...test setup and cases to be implemented...
+});
