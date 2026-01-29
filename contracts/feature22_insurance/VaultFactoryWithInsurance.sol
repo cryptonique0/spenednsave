46b387cdb709780523c8e1c736a4d9ac4a7e3dea
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 00:27:12 +0100

    chore: add SpendVaultWithInsurance and VaultFactoryWithInsurance contracts for insurance integration

---
+++ contracts/feature22_insurance/VaultFactoryWithInsurance.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title VaultFactoryWithInsurance
+/// @notice Factory for deploying insurance-enabled vaults
+contract VaultFactoryWithInsurance {
+    // ...core logic to be implemented...
+}
