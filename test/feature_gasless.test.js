9c63f6428177253ade5d546f4dc660c4f9b4de68
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-30 01:17:21 +0100

    fix: update import statements to use ES6 syntax in feature_gasless.test.js

--- test/feature_gasless.test.js
+++ test/feature_gasless.test.js
@@ -1,5 +1,6 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+
+import { expect } from "chai";
+import { ethers } from "hardhat";
 
 describe("CompliancePlugin & SpendVaultWithCompliance", function () {
   let CompliancePlugin, SpendVaultWithCompliance;
